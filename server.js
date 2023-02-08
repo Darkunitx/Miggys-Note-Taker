9dbb32cfe1d8e1d964a8e6c9c62e671d0218ce31
Darkunitx <darkunitx@live.com>
2023-02-07 21:29:29 -0600

    connected routes index file to server.js and is now being called on the main js

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const path = require('path');
+const routes = require('./routes/index.js');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/')));
+app.use('/', routes);
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
 app.listen(PORT, () =>
 console.log(`Example app listening at http://localhost:${PORT}`)
